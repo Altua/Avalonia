74366f40493b3871a8954ec6b7aa40582d898849
jankrib <jan.bjerke@gmail.com>
2023-07-11 10:47:54 +0200

    Initial setup

---
+++ samples/FontExplorer/App.xaml.cs
@@ -0,0 +1,32 @@
+using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace FontExplorer
+{
+    public class App : Application
+    {
+        public App()
+        {
+        }
+
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime)
+            {
+                desktopLifetime.MainWindow = new MainWindow();
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+
+    }
+}
---
+++ samples/FontExplorer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Platform;
+
+namespace FontExplorer
+{
+    internal class Program
+    {
+        [STAThread]
+        public static int Main(string[] args)
+            => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+
+        /// <summary>
+        /// This method is needed for IDE previewer infrastructure
+        /// </summary>
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .LogToTrace()
+                .UsePlatformDetect();
+
+    }
+}
