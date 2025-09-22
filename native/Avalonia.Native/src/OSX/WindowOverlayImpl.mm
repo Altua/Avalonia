d980b150374f51b2e047469357c591639984cde0
Adarsh <adarshb@99x.io>
2025-09-22 14:42:50 +0530

    Added a comment to warn about potential issue.

--- native/Avalonia.Native/src/OSX/WindowOverlayImpl.mm
+++ native/Avalonia.Native/src/OSX/WindowOverlayImpl.mm
@@ -127,6 +127,10 @@ WindowOverlayImpl::WindowOverlayImpl(void* parentWindow, char* parentView, IAvnW
     }];
     
     // Special key codes that need explicit sending to AvnView
+    // IMPORTANT:
+    // Careful while adding keys to this list.
+    // The keys are directly sent to AvnView, so PowerPoint will not get a chance to handle them.
+    // As a result keys in this list will not be handled by PowerPoint if Grunt object is selected.
     static const std::unordered_set<unsigned short> specialKeyCodes = {
         11,  // Cmd+b (Bold)
         34,  // Cmd+I (Italic)
